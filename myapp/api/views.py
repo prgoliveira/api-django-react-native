ce7cee3b063d2bdb0b834f9fbc521e9a99daa4c8
Gabriel Oliveira <prg.oliveirag@gmail.com>
2021-05-15 12:54:47 -0300

    feat: first model and serialization
    simple model and serialization for api testing

--- myapp/api/views.py
+++ myapp/api/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .models import Article
+from .serializers import ArticleSerializer
+from django.http.response import HttpResponse, JsonResponse
+from django.shortcuts import render, HttpResponse
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+def Index(request):
+    return HttpResponse('its working')
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def article_list(request):
+
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many= True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        else:
+            return JsonResponse(serializer.errors, status=400)
